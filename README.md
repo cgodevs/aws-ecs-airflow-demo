f84439f8eab2fbb18aab761da73c1c5268a6d0c5
kharo <kharol.go@gmail.com>
2025-02-23 17:41:50 -0300

    v6

--- README.md
+++ README.md
@@ -9,11 +9,13 @@ We'll set up an AWS environment to deploy every new commit of a github repositor
 Deploy all versions of a github repo as zip files in an s3 bucket, then call a lambda to get the newest version of the zipped file within it into an EFS configured within the Lamda function.
 
 ## Steps
-- Criar repo (OK)
-- Conectar AWS ao GitHub através do app na conta (OK)
-- Criar S3 bucket para armazenar versões do repo (OK)
-- Criar EFS (OK)
-- Criar Role Especial com Inline Policy acrescentada para Lambda poder ler o EFS e EC2 (OK)
-- Criar Security groups de acesso entre Lambda, ECS e EFS (OK)
-- Criar Codepipeline (OK)
+- Criar repo
+- Conectar AWS ao GitHub através do app na conta
+- Criar S3 bucket para armazenar versões do repo
+- Criar EFS e EFS Point (algumas configurações importantes)
+- Criar Role Especial com Inline Policy acrescentada para Lambda poder ler o EFS e EC2
+  - ou AmazonElasticFileSystemClientReadWriteAccess e AWSLambdaBasicExecutionRole
+- Criar Security groups de acesso entre Lambda, ECS e EFS
+- Criar Codepipeline
+- Criar VPC Endpoint para que a Lambda tenha acesso ao S3 (caso ela e o EFS estiverem em uma mesma VPC, ao contrário do S3, que nunca está)
 - Criar Lambda para salvar versões do repo no S3 e EFS 
--- lambda_function.py
+++ lambda_function.py
@@ -6,8 +6,10 @@ import io
 # AWS Clients
 s3 = boto3.client('s3')
 
-# Define where EFS is mounted inside Lambda
+# Define constants
 EFS_MOUNT_PATH = "/mnt/artifacts"
+DEFAULT_BUCKET_NAME = "main-branch-airflow-ecs-demo-copies"
+DEFAULT_FILE_NAME = DEFAULT_BUCKET_NAME + "/aws-ecs-airflow-demo.zip"
 
 
 def get_latest_s3_file(bucket_name):
@@ -24,28 +26,40 @@ def get_latest_s3_file(bucket_name):
     return sorted_files[0]['Key']
 
 
+def download_s3_file(bucket_name, object_key):
+    try:
+        print(f"Processing file: {object_key} from bucket: {bucket_name}")
+        s3_object = s3.get_object(Bucket=bucket_name, Key=object_key)
+        zip_content = io.BytesIO(s3_object['Body'].read())
+        return zip_content
+    except Exception as e:
+        raise Exception({'statusCode': 500, 'body': f"Error downloading {object_key} from S3.", 'error': str(e)})
+
+def extract_to_efs(zip: io.BytesIO):
+    try:
+        with zipfile.ZipFile(zip, 'r') as zip_ref:
+            zip_ref.extractall(EFS_MOUNT_PATH)
+        print(f"Extracted object to {EFS_MOUNT_PATH}")
+    except Exception as e:
+        raise Exception({'statusCode': 500, 'body': f"Error extracting object.", 'error': str(e)})
+
 def lambda_handler(event, context):
-    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    # Try to extract bucket and object key from event
+    bucket_name = event.get('Records', [{}])[0].get('s3', {}).get('bucket', {}).get('name', DEFAULT_BUCKET_NAME)
+    object_key = event.get('Records', [{}])[0].get('s3', {}).get('object', {}).get('key')
 
-    # Get the latest file from S3
-    latest_file_key = get_latest_s3_file(bucket_name)
-    if not latest_file_key:
-        print("No files found in S3 bucket.")
-        return
+    # If no object key is provided (manual trigger), get the latest file
+    if not object_key:
+        print("No specific file in event. Fetching the latest file from S3...")
+        object_key = get_latest_s3_file(bucket_name) or DEFAULT_FILE_NAME
+        print(f"Latest file in S3: {object_key}")
+        if not object_key:
+            print("No files found to process.")
+            return {'statusCode': 404, 'body': "No files available in S3."}
 
-    print(f"Processing latest file: {latest_file_key}")
-
-    # Download the ZIP file
-    s3_object = s3.get_object(Bucket=bucket_name, Key=latest_file_key)
-    zip_content = io.BytesIO(s3_object['Body'].read())
-
-    # Extract the contents to EFS
-    with zipfile.ZipFile(zip_content, 'r') as zip_ref:
-        zip_ref.extractall(EFS_MOUNT_PATH)
-
-    print(f"Extracted {latest_file_key} to {EFS_MOUNT_PATH}")
-
+    zip_content = download_s3_file(bucket_name, object_key)
+    extract_to_efs(zip_content)
     return {
         'statusCode': 200,
-        'body': f"Successfully processed {latest_file_key}"
+        'body': f"Successfully processed {object_key}"
     }
